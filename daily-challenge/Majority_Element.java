ea68d5df0873c5a0ae0c0b966c35b21ee4f46dfe
phduchuy <phanhoangduchuyy@gmail.com>
2024-06-20 21:25:12 +0700

    daily-challenge-19-06

---
+++ daily-challenge/Majority_Element.java
@@ -0,0 +1,11 @@
+public class Majority_Element {
+    public static int majorityElement(int[] nums) {
+        float times = nums.length / 2;
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- daily-challenge/Set_Mismatch.java
+++ daily-challenge/Set_Mismatch.java
@@ -1,7 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Set_Mismatch {
 
     public static int[] findErrorNums(int[] nums) {
+        int[] res = new int[2];
 
+        Set<Integer> set = new HashSet<>();
+
+        for (Integer i : nums) {
+            if (set.contains(i)) {
+                res[0] = i;
+            }
+            set.add(i);
+        }
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                res[1] = i;
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
